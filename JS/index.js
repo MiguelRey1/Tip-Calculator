92991bb4cdd84280f5143ba6d4fb282d8a6b9a2c
Miguel E. Reynoso <miguelrey003@gmail.com>
2021-09-09 15:59:33 -0400

    Final Stage, app done

---
+++ JS/index.js
@@ -0,0 +1,161 @@
+const form = document.getElementById('form');
+
+/* Span with the text warning */
+const textWarning = document.getElementById('warningText');
+
+/* input for putting the amount of bill and the number of the people */
+const bill = document.getElementById('bill');
+const numberPeople = document.getElementById('people');
+
+/* Buttons to select the percent */
+
+const buttons = document.getElementById('buttons');
+const btn5 =document.getElementById('btn5');
+const btn10 =document.getElementById('btn10');
+const btn15 =document.getElementById('btn15');
+const btn25 =document.getElementById('btn25');
+const btn50 =document.getElementById('btn50');
+const custom = document.getElementById('input-custom');
+
+/* Span where the result will be display */
+const totalPerPerson = document.getElementById('total-amount-person');
+const totalAmount = document.getElementById('tip-amount-result');
+
+let buttonsValue;
+
+/* Functions to calculate the result */
+const calculate = (bill, percentTip, people)=>{
+    let percent = (bill * percentTip) / 100;
+    let result= (percent + bill )/ people;
+    return result.toFixed(2);
+}
+
+const calculateAmountTip =(bill, percentTip, people) => {
+    let percent = (bill * percentTip)/100;
+    let result = percent / people;
+    return result.toFixed(2);
+}
+
+bill.addEventListener('keyup',(event)=>{
+    let value1 = event.target.value;
+    let value2 = event.target.valueAsNumber;
+
+    if (value1 == "" || value2 == NaN || value2 == 0) {
+        numberPeople.setAttribute("disabled", "");
+    }else{
+        numberPeople.removeAttribute('disabled');
+    }
+    
+
+})
+
+buttons.addEventListener('click', (event)=>{
+    let value = event.target.textContent;
+    let children = Array.from(buttons.children);
+    if (value != "") {
+        buttonsValue = parseInt(value);
+    }
+
+    switch (value) {
+        case "5%":
+
+        for (const child of children) {
+                if (child.textContent !== value &&  child.classList.contains("visited")) {
+                    child.classList.remove("visited")  
+                } else {
+                    btn5.classList.add("visited")
+                }
+            }
+        break;
+        case "10%":
+            for (const child of children) {
+                if (child.textContent !== value &&  child.classList.contains("visited")) {
+                    child.classList.remove("visited")
+                } else {
+                    btn10.classList.add("visited")
+                }
+            }
+        break;
+        case "15%":
+            for (const child of children) {
+                if (child.textContent !== value &&  child.classList.contains("visited")) {
+                    child.classList.remove("visited")
+                } else {
+                    btn15.classList.add("visited")
+                }
+            }
+        break;
+        case "25%":
+            for (const child of children) {
+                if (child.textContent !== value &&  child.classList.contains("visited")) {
+                    child.classList.remove("visited")
+                } else {
+                    btn25.classList.add("visited")
+                }
+            }
+        break;
+        case "50%":
+            for (const child of children) {
+                if (child.textContent !== value &&  child.classList.contains("visited")) {
+                    child.classList.remove("visited")
+                } else {
+                    btn50.classList.add("visited")
+                }
+            }
+        break;
+    }
+    
+})
+
+custom.addEventListener('keyup', (event)=>{
+    if(event.target.value != ""){
+        buttonsValue = parseInt(event.target.value);
+    }
+
+});
+
+
+numberPeople.addEventListener('keyup', (event)=>{
+
+    let billValue = bill.valueAsNumber;
+    let peopleNumber = event.target.valueAsNumber;
+    let peopleValue = event.target.value;
+
+    if (peopleNumber == 0 || peopleValue =="") {  
+        textWarning.textContent = "can't be zero";
+        textWarning.setAttribute("class","warning-text");
+        numberPeople.classList.add("warning");
+        
+    } else if(buttonsValue == 0 ){
+        textWarning.textContent = "";
+        textWarning.removeAttribute('class');
+        numberPeople.classList.remove('warning');
+        totalPerPerson.textContent = (billValue / peopleNumber).toFixed(2);
+
+    } else if(buttonsValue !== undefined && buttonsValue !=0 ){
+        totalAmount.textContent = calculateAmountTip(billValue, buttonsValue, peopleNumber);
+        totalPerPerson.textContent = calculate(billValue, buttonsValue, peopleNumber );
+
+    }else {
+        textWarning.textContent = "";
+        textWarning.removeAttribute('class');
+        numberPeople.classList.remove('warning');
+        totalPerPerson.textContent = (billValue / peopleNumber).toFixed(2);
+    }
+
+})
+
+/* Button for reset all fields */
+form.addEventListener('reset', ()=>{
+    textWarning.textContent = "";
+    textWarning.removeAttribute('class');
+    numberPeople.classList.remove('warning');
+    numberPeople.setAttribute("disabled", "");
+    buttonsValue = ""; 
+    totalAmount.textContent = "0.00";
+    totalPerPerson.textContent = "0.00";
+    let children = Array.from(buttons.children);
+    for (const child of children) {
+        child.classList.remove("visited");
+    }
+})
